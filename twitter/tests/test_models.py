93ec069ae1394f7ba627b92d3c67731cf4d5e832
jack <smirniyhr@gmail.com>
2016-06-15 09:05:55 +0300

    Third start

--- twitter/tests/test_models.py
+++ twitter/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from twitter.models import Tweet
 
-class SoloModelTestCase(TestCase):
+class TweetModelTestCase(TestCase):
 
 	def setUp(self):
 		user = get_user_model().objects.create_user('dummy')
